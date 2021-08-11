36869670ec682ac5b04e16fac95fb70096149db5
donghyuk91.kim <donghyuk91.kim@navercorp.com>
2021-08-11 10:19:52 +0900

    [main] add set_addr method

--- utils.py
+++ utils.py
@@ -2,6 +2,7 @@
 
 class MyUtils:
     def __init__(self):
+        self.address = None
         self.name = None
 
     def set_name(self, name):
@@ -9,3 +10,9 @@ class MyUtils:
 
     def get_name(self):
         return self.name
+
+    def set_addr(self, addr):
+        self.address = addr
+
+    def get_addr(self):
+        return self.address
